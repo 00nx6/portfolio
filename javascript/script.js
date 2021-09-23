31f5dc445dfe63ada42e138e750d9e2948fc4324
00nx6 <77593315+00nx6@users.noreply.github.com>
2021-09-23 15:37:29 +0100

    re done javascript - original too messy

---
+++ javascript/script.js
@@ -0,0 +1,36 @@
+// arrow down animation
+
+var extend_arrow = KUTE.to (
+    '#arrow-line',
+    {height: '45'},
+    {duration: 2000, yoyo:true, repeat: 99999}
+).start();
+
+var arrow_head = KUTE.to (
+    '#arrow-head',
+    { translateY: 24},
+    {duration: 2000, yoyo:true, repeat: 99999}
+).start();
+
+// blob animation
+
+var morph = KUTE.to (
+    '#blob1',
+    {path: '#blob3'},
+    {duration: 8000, yoyo: true, repeat:99999}
+).start();
+
+const blobCont = document.querySelector(".blobCont");
+const blobs = Array.from(blobCont.children);
+var blobsVis = [];
+function getBlobs() {
+    for(let i = 0; i < blobs.length - 1; i++) {
+        if(blobs[i].classList.contains('hidden') !== true) {
+            blobsVis.push(blobs[i].children)
+            if(blobsVis[i][i])
+        };
+    };
+};
+getBlobs();
+
+console.log(blobsVis)
