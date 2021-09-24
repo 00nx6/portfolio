a861d6f8617574b165756e06b78717d362072adf
00nx6 <77593315+00nx6@users.noreply.github.com>
2021-09-24 12:46:51 +0100

    Complete rework.

--- javascript/script.js
+++ javascript/script.js
@@ -1,38 +1,22 @@
-// arrow down animation
+var stars = document.getElementById('stars');
+var moon = document.getElementById('moon');
+var mountainBhnd = document.getElementById('mountain_behind');
+var mountainFrnt = document.getElementById('mountain_front');
+var myName = document.getElementById('name');
 
-var extend_arrow = KUTE.to (
-    '#arrow-line',
-    {height: '45'},
-    {duration: 2000, yoyo:true, repeat: 99999}
-).start();
 
-var arrow_head = KUTE.to (
-    '#arrow-head',
-    { translateY: 24},
-    {duration: 2000, yoyo:true, repeat: 99999}
-).start();
 
-// blob animation
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    myName.style.marginRight = value + 'px';
+    moon.style.top = (value * 1) * 0.45  + 'px';
+})
 
-var morph = KUTE.to (
-    '#blob1',
-    {path: '#blob3'},
-    {duration: 8000, yoyo: true, repeat:99999}
-).start();
-
-const blobCont = document.querySelector(".blobCont");
-const blobs = Array.from(blobCont.children);
-var blobsVis = [];
-function getBlobs() {
-    for(let i = 0; i < blobs.length - 1; i++) {
-        if(blobs[i].classList.contains('hidden') !== true) {
-            blobsVis.push(blobs[i].children)
-            if(blobsVis[i][i]) {
-                
-            }
-        };
-    };
-};
-getBlobs();
-
-console.log(blobsVis)
+var hero = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#pageWrap",
+    pin:true,
+    start: "top top",
+    end: "+=800",
+  }
+});
